4f87588bb12ba2d53a4ac68074e069b1d5612182
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-04-02 14:52:55 +0900

    build.gradleのnexusPublishing設定をGitHub Packagesに変更し、OSGPlayerUtilsクラスを追加しました。

---
+++ src/main/java/com/karasu256/one_shot_glory/util/OSGPlayerUtils.java
@@ -0,0 +1,41 @@
+package com.karasu256.one_shot_glory.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+/**
+ * One-Shot-Gloryのプレイヤーに関するユーティリティクラス
+ * 
+ * @author Hashibutogarasu
+ * @version 1.0
+ */
+public class OSGPlayerUtils {
+    /** One-Shot-Gloryのシステムが有効なプレイヤーのリスト */
+    private static List<Player> enabledList = new ArrayList<>();
+    
+    /**
+     * 特定のプレイヤーのみにこのプラグインのシステムを有効化させます。
+     * @param player 有効にするプレイヤー
+     */
+    public static void enableOneShotSystemForPlayer(Player player){
+
+    }
+
+    /**
+     * 特定のプレイヤーのみにこのプラグインのシステムを無効化させます。
+     * @param player 無効にするプレイヤー
+     */
+    public static void disableOneShotSystemForPlayer(Player player){
+
+    }
+
+    /**
+     * 有効化されているプレイヤーのリストを取得します。
+     * @return プレイヤーのリスト
+     */
+    public static List<Player> getEnabledList() {
+        return enabledList;
+    }
+}
