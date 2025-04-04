4866faf3063523203d8f869486e9c5f02d876eb1
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-04-04 22:07:32 +0900

    プレイヤーのゲームモードチェックの条件を修正し、観戦者モードのプレイヤーに対するターゲット生成を適切に制御

--- src/main/java/com/karasu256/one_shot_glory/util/GameManager.java
+++ src/main/java/com/karasu256/one_shot_glory/util/GameManager.java
@@ -72,7 +72,7 @@ public class GameManager {
             return;
         }
 
-        if (player.getGameMode() != GameMode.SPECTATOR) {
+        if (player.getGameMode() == GameMode.SPECTATOR) {
             return;
         }
 
